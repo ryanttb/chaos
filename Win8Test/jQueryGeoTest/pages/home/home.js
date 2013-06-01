a4f99cb51fe90463a93143a9cf9869ed16111dbd
Ryan Westphal <ryan@trippingthebits.com>
2013-06-01 15:58:59 -0400

    first working win8 map

---
+++ Win8Test/jQueryGeoTest/pages/home/home.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    var map = null;
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+           if (!map) {
+             map = $("#map").geomap({
+               zoom: 3
+             });
+
+           }
+        }
+    });
+})();
