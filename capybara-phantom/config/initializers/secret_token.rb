f2471c4cdf267fb62d2dc0f814dd56b11ba5fd0d
Ryan Westphal <ryan@trippingthebits.com>
2013-09-11 19:20:45 -0400

    rails new -T; install rspec, capybara

---
+++ capybara-phantom/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraPhantom::Application.config.secret_token = 'b8123a6f1465abb8dfaf80db0a73c2c92638cad4d19e8ea6e1f65c8dba64984b6a6dd9347ee0f36d96bce6d784c0406d518e2225866a19cd3b7941e669ff9d44'
